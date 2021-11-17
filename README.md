4ba62d719394f8bd521fc9173b221bf88fbc3c71
wenhaofang <wenhaofang1998@qq.com>
2021-11-17 23:52:32 +0800

    add: data process

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This repository includes some demo Seq2Seq models.
 
 Note: The project refers to <https://github.com/bentrevett/pytorch-seq2seq>
+
+### Prepare Data
+
+```shell
+PYTHONPATH=. python dataprocess/process.py
+```
---
+++ utils/parser.py
@@ -0,0 +1,23 @@
+import argparse
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+
+    # For Basic
+    parser.add_argument('--name', default = 'main', help = '')
+
+    # For Data
+    parser.add_argument('--sources_path', default = 'datasources', help = '')
+    parser.add_argument('--targets_path', default = 'datatargets', help = '')
+
+    parser.add_argument('--train_file', default = 'train_data.tsv', help = '')
+    parser.add_argument('--valid_file', default = 'valid_data.tsv', help = '')
+    parser.add_argument('--test_file' , default = 'test_data.tsv' , help = '')
+
+    parser.add_argument('--min_freq', type = int, default = 7, help = '')
+    parser.add_argument('--max_numb', type = int, default = 30000, help = '')
+    parser.add_argument('--max_seq_len', type = int, default = 16, help = '')
+
+if __name__ == '__main__':
+    parser = get_parser()
+    option = parser.parse_args()
